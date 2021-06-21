ca5c657c2a2dc96ea3ad9e0d46ca8dd42d076184
Antti Sivula <antti.j.sivula@gmail.com>
2021-06-21 23:43:13 +0300

    Refactor c1e5 to use constants

--- chapter-1/exercise-5.c
+++ chapter-1/exercise-5.c
@@ -3,8 +3,12 @@
 /* print Fahrenheit-Celsius table
     in reverse order */
 
+#define LOWER 0
+#define UPPER 300
+#define STEP  20
+
 int main() {
-    for (int fahr = 300; fahr >= 0; fahr -= 20) {
+    for (int fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
         printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
     }
 }

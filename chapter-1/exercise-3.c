c0027efdc7507e061a293e26958795e278c00483
Antti Sivula <antti.j.sivula@gmail.com>
2021-06-21 20:04:16 +0300

    Add solution for c1e3

---
+++ chapter-1/exercise-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahrenheit\tCelsius\n");
+    printf("----------\t-------\n");
+
+    while (fahr <= upper) {
+        celsius = (5.0/9.0) * fahr;
+        printf("%.2f\t\t%.2f\n", fahr, celsius);
+        fahr += step;
+    }
+}

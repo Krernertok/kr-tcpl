d3401d0b94fa94dc0c97dd2a5b521efaa66e364b
Antti Sivula <antti.j.sivula@gmail.com>
2023-08-05 23:49:39 +0300

    Add solution for exercise 1.17

--- chapter-1/exercise-16.c
+++ chapter-1/exercise-16.c
@@ -6,64 +6,50 @@ int get_next_line(char line[], int line_length);
 void copy(char to[], char from[]);
 
 int main() {
-	int len;
-	int prev_len = 0;
-	int max = 0;
-	char line[MAXLINE];
-	char longest[MAXLINE];
-	
-	while ((len = get_next_line(line, MAXLINE)) > 0) {
-		// 1. line does not end
-		//		a. there is previous not ended line
-		//		b. there is no previous not ended line
-		//		DOESN'T matter -> append len to prev_len
-		if (line[len] != '\0') {
-			prev_len += len;
-			continue;
-		}
-		// 2. line ends
-		//		a. there is previously not ended line
-		//			prev_len != 0
-		//		b. there is no previously not ended line
-		//			prev_len == 0
-		if (prev_len != 0) {
-			len += prev_len;
-			prev_len = 0;
-		}
+    int len;
+    int max = 0;
+    char line[MAXLINE];
+    char longest[MAXLINE];
 
-		if (len > max) {
-			max = len;
-			copy(line, longest);
-		}
-	}
+    while ((len = get_next_line(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(line, longest);
+        }
+    }
 
-	printf("Longest line: %d\nLine:\n%s\n", max, longest);
+    printf("Longest line: %d\nLine:\n%s\n", max, longest);
 }
 
-int get_next_line(char line[], int len) {
-	int c, i;
+int get_next_line(char line[], int max_len) {
+    int c;
+    int i = 0;
 
-	for (i = 0; i < (len - 1) && ((c = getchar()) != EOF && c != '\n'); ++i) {
-		line[i] = c;
-	}
+    while ((c=getchar())!=EOF && c!='\n') {
+        if (i < max_len-1) {
+            line[i] = c;
+        }
+        ++i;
+    }
 
-	if (c != EOF && c != '\n') {
-		return i;
-	}
+    if (c == '\n' && i < max_len) {
+        line[i] = c;
+        ++i;
+    }
 
-	if (c == '\n') {
-		line[i] = c;
-		++i;
-	}
+    if (i >= max_len) {
+        line[max_len-1] = '\0';
+    } else {
+        line[i] = '\0';
+    }
 
-	line[i] = '\0';
-	return i;
+    return i;
 }
 
 
 void copy(char from[], char to[]) {
-	int i = 0;
-	while((to[i] = from[i]) != '\0') {
-		++i;
-	}
+    int i = 0;
+    while((to[i] = from[i]) != '\0') {
+        ++i;
+    }
 }
